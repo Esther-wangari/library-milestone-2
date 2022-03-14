461acc2eabd3c25826683eabfd8b777a8cffb26b
Esther-wangari <88084558+Esther-wangari@users.noreply.github.com>
2022-03-15 00:37:18 +0300

    Add files via upload

---
+++ main project milestone 1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int num1,num2,num3,num4,num5,num6,num7;
+    char changepassword,previous password,new password;
+	printf("county library: \n");
+	printf(" welcome Miss Esther \n");
+	printf("what would you like to do?: \n");
+	printf("1. view user\n");	
+	printf("2. Add new user\n");	
+    printf("3. Edit user\n");
+    printf("4. delete user\n");
+    printf("5. change password\n"); 
+    printf("6. log out\n");
+    printf("7. exit system\n");
+    printf("select any number");
+    scanf("%s",&num5);
+    print("change password");
+    scanf("%s",&changepassword);
+    print("enter previous password");
+    printf("%s",&previouspassword);
+    printf("enter new password");
+    scanf("%s",&new password);
+    printf("confirm new password");
+    scanf("%s",&confirm new password);
+
+
+	return 0;
+}
